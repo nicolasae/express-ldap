644330b11b08bc3350d72c86351d62b1f66e929e
nicolasae <nicolas.aguirre@utp.edu.co>
2023-04-24 10:40:03 -0500

    Enable CORS

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session = require('express-session');
 const methodOverride = require('method-override');
+const cors = require('cors')
 
 /*** Main Router (require) */
 const mainRoutes = require("./routes/mainRoutes")
@@ -22,6 +23,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 app.use(cookieParser());
+app.use(cors())
 app.use(express.static(path.join(__dirname, "../public/")));
 
 // ************  Middlewares ************/
--- src/database/config/ldap.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    url:'ldap://ldap.utp.edu.co:778',
-    adminDn: 'uid=consultasweb,dc=utp',
-    adminPassword: 'c0ns4lt4s',
-    userSearchBase: 'dc=utp',
-    usernameAttribute: 'uid',
-}
